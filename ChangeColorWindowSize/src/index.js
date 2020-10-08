96f53829cd573b435226fc22b7d419a86e9cfb6b
이정석 <jsuk10@naver.com>
2020-10-08 16:32:56 +0900

    create : ChangeColor Depends on Web Size
    사이트 크기에 따라 색을 다르게 할당

---
+++ ChangeColorWindowSize/src/index.js
@@ -0,0 +1,22 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+import "./styles.css";
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+const body = document.querySelector("body");
+
+function ChangeColor() {
+  const size = window.innerWidth;
+  console.log(size);
+  if (size > 900) {
+    console.log("big");
+    body.style.background = "yellow";
+  } else if (size < 700) {
+    console.log("small");
+    body.style.background = "blue";
+  } else {
+    console.log("nomal");
+    body.style.background = "purple";
+  }
+}
+
+window.addEventListener("resize", ChangeColor);
