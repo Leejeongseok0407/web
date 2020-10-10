381193a3ff06f24557fb9eb85f3de8b002c87140
이정석 <jsuk10@naver.com>
2020-10-10 21:39:39 +0900

    feat : Clock + Name
    in put name and save in localStorage

---
+++ ClockName/src/index.js
@@ -0,0 +1,30 @@
+
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+const Week = new Array("일", "월","화","수","목","금","토");
+
+function getTime() {
+  const nowTime = new Date();
+
+  const month = nowTime.getMonth()+1;
+  const day = nowTime.getDate();
+  const minutes = nowTime.getMinutes();
+  const hours = nowTime.getHours();
+  const seconds = nowTime.getSeconds();
+  const dayOfWeek = nowTime.getDay();
+
+
+
+  clockTitle.innerText =
+  `${hours < 10 ? `0${hours}` : hours}: ${
+    minutes < 10 ? `0${minutes}` : minutes}: ${
+    seconds < 10 ? `0${seconds}` : seconds}`;
+  
+}
+
+function init() {
+  getTime();
+  setInterval(getTime,1000);
+}
+
+init();
